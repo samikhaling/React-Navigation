ea7b976998a4109cae384d47868413eec20cc854
Samir Rai <samikhaling14@gmail.com>
2023-06-18 20:14:39 -0400

    updated project

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from '../components/Table';
+// import Table from '../components/Table';
+import SortableTable from '../components/SortableTable';
 
 function TablePage() {
   const data = [
@@ -12,6 +13,7 @@ function TablePage() {
     {
       label: 'Name',
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: 'Color',
@@ -20,6 +22,7 @@ function TablePage() {
     {
       label: 'Score',
       render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -29,7 +32,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
